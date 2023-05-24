68c88b4ba08f65940626a15d6cc1cf45a473d425
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-24 12:38:46 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path
   end
-  
+
   private
 
   def user_params
