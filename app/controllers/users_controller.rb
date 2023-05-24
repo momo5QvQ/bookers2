b048eacc38c7f4f70e30a63a44915096f06d5e5d
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-24 04:39:11 +0000

    [add]newbookのview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def edit
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
