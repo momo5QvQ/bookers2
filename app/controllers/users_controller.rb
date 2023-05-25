bf3b555a8c05a463c9f56552c20e1fa33c3aaac6
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-25 11:43:19 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all(params[:id])
+  
+    
+  end
+  
   def show
     @user = User.find(params[:id])
     @book = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
