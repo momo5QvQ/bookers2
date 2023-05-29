395acf3ab99eecb075aa514180c43e9af13f7d07
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-29 08:03:23 +0000

    [add]バリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+     redirect_to book_path(@book)
+    else
+     @books = Book.all
+     @user = current_user
+     render :index
+    end
   end
 
   def index
@@ -28,8 +33,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
